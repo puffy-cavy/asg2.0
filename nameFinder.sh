b92938d51dfe57dc94913126ed578da67e4c3ec9
ziwei-zhu <zzhu39@illinois.edu>
2019-12-16 18:40:09 -0600

    jenkinsfile

--- nameFinder.sh
+++ nameFinder.sh
@@ -3,6 +3,6 @@
 export PATH=/usr/local/bin:$PATH
 
 # outputStacks=`aws cloudformation describe-stacks --query 'Stacks[*].[StackName]' --output text | grep -w ${ENV_NAME}.*${APP_NAME}.*cluster`
-STACK_LIST=($(aws cloudformation describe-stacks --query 'Stacks[*].[StackName]' --output text | grep -w ${ENV_NAME}.*cluster))
+STACK_LIST=($(aws cloudformation describe-stacks --query 'Stacks[*].[StackName]' --output text | grep -w ${ENV_NAME}-.*-cluster))
 echo ${STACK_LIST}
 
